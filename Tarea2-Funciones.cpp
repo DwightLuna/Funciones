d519be42b960f7f168ccc58a15851a4263584231
DwightLuna <79483217+DwightLuna@users.noreply.github.com>
2021-03-03 13:47:17 -0600

    Add files via upload

---
+++ Tarea2-Funciones.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+using std::cout;
+using std::cin;
+    int main()
+    {
+        int base,altura,area;
+        cout<<"Programa para calcular el area de un rectangulo"<< endl;
+        cout<<"Introduce la base del rectagulo a calcular: "<< endl;
+        cin>>base;
+        cout<<"Introduce la altura del rectangulo: "<< endl;
+        cin>>altura;
+        area =base*altura;
+        cout<<"El area del rectangulo es: "<<area<<endl;
+
+
+    }
